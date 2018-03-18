911a7ce2ab7ae1b0f224d6f4abdb0946d6c26f15
Monty Sinha <monty@vayaconnect.com>
2018-03-18 22:05:40 +0000

    Add files via upload

---
+++ OnlineRegForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieListingsApp.CustomerClasses
+{
+    public class OnlineRegForm
+    {
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
